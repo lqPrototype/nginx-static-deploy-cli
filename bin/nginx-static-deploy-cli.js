a49621938f2f84d40876cf040abc802fe24ef196
梁强 <1049229070@qq.com>
2023-10-11 10:25:20 +0800

    feat: add check version

--- bin/nginx-static-deploy-cli.js
+++ bin/nginx-static-deploy-cli.js
@@ -1,10 +1,19 @@
 #!/usr/bin/env node
 
 const Service = require('../lib/service')
+const { error } = require('../lib/utils')
 const service = new Service()
 
 const rawArgv = process.argv.slice(2)
 const args = require('minimist')(rawArgv)
 const command = args._[0]
 
-service.run(command, args, rawArgv)
+service
+  .run(command, args, rawArgv)
+  .then(() => {
+    //
+  })
+  .catch(() => {
+    error(e)
+    process.exit(1)
+  })
